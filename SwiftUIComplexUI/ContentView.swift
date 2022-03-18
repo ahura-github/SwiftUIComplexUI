eeeb3636a35cc502250de052e6feea20079d1751
Azhman <bapir.dev@gmail.com>
2022-03-18 15:50:09 +0330

    Initial Commit

---
+++ SwiftUIComplexUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIComplexUI
+//
+//  Created by Amirreza Zarepour on 3/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIComplexUI/SwiftUIComplexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComplexUIApp.swift
+//  SwiftUIComplexUI
+//
+//  Created by Amirreza Zarepour on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComplexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
